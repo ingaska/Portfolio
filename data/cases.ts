b73035038a71e2a58ddb1e08f0796f207abffc33
INGA SUMSKA <ingaska@INGAs-MacBook-Pro.local>
2026-06-18 08:57:01 +0300

    Add second Infographic slide to Femia Brand case

--- data/cases.ts
+++ data/cases.ts
@@ -353,6 +353,7 @@ export const cases: Case[] = [
       '2949:30236', // Photos — 2
       '2949:30817', // Photos — Relationship and Pregnancy
       '2949:30652', // Infographic
+      '2949:30583', // Infographic — 2
       '2949:28342', // In use — Text and Picture
       '2949:28327', // In use — Photo guidelines
     ],
